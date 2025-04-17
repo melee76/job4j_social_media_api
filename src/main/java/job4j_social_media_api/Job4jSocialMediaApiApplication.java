e8ce07af1500a183fe16a1779c4528e232608c1e
zoubarevna <melee76@yandex.ru>
2025-04-17 12:09:37 +0300

    first commit

---
+++ src/main/java/job4j_social_media_api/Job4jSocialMediaApiApplication.java
@@ -0,0 +1,13 @@
+package job4j_social_media_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jSocialMediaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Job4jSocialMediaApiApplication.class, args);
+	}
+
+}
